e5d08906a206ba173d33e27a9375f9693fb049a7
Dale Harris <7697179+daleharris541@users.noreply.github.com>
2023-04-24 17:10:50 -0700

    Modified build order to include new entries

--- tools/jsonParser.py
+++ tools/jsonParser.py
@@ -4,18 +4,22 @@ def makeBuildOrder(filepath):
     build_order = []
     with open(filepath) as f:
         data = json.load(f)
-
-    build = data['buildings']
-
+    #this would be somewhere we could import a ton of different strategies
+    #and pick the build order randomly
+    build = data['quickbuild']
     for key in build:
-        unit = key['unit']
+        unit = key['name']
+        unitId = key['id']
+        unitType = key['type']
         action =  key['action']
-        atSupply = key['atSupply']
+        atSupply = key['supply']
+        time = key['time']
+        frame = key['frame']
         if 'targetQuantity' in key:
             targetQuantity = key['targetQuantity']
         else:
             targetQuantity = None
         
-        build_order.append([unit, action, atSupply, targetQuantity])    
+        build_order.append([unit, unitId, unitType, action, targetQuantity, atSupply, time, frame])    
         #print(atSupply, type, unit, targetQuantity)
     return build_order
