b95021bff56842553a445daceef1d63e82ed7c02
TheSyes <philipphhmnn@gmail.com>
2023-04-25 19:16:52 +0200

    Added a empty bot template

--- BuildManager.py
+++ BuildManager.py
@@ -2,14 +2,18 @@ from tools import makeBuildOrder
 from sc2.unit import Unit
 from sc2.units import Units
 
-#this is absolutely trash and only for debugging
+#This class should do the following.
+#   -Execute Build order.
+#   -Track Build order progress
+#   -Building placement.
+#   -Rebuild destroyed structures
+#   -Send workers back to Comand Center / Mining
 
-def getBuildOrder():
-    return makeBuildOrder('tools/test.json')
 
-def printOrder():
+class BuildManager:
+    def getBuildOrder():
+        return makeBuildOrder('tools/test.json')
+
     order = getBuildOrder()
     for row in order:
         print(row)
-
-printOrder()
