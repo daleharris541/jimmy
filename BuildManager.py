a1c9791724fbfc0a4b29bd975516aa390768f536
Dale Harris <7697179+daleharris541@users.noreply.github.com>
2023-04-27 18:27:24 -0700

    Modified Build Manager to work as a function

--- BuildManager.py
+++ BuildManager.py
@@ -11,59 +11,71 @@ from sc2.units import Units
 #   -Building placement.
 #   -Rebuild destroyed structures
 
-class BuildManager():
-    def __init__(self, bot: BotAI):
-       self.bot = bot
-        #self.queue = [["SUPPLYDEPOT", "build", 15, 1], ["BARRACKS", "build", 15, 1]]
-        #self.build_progress()
+async def execute_build_queue(self):
+    queue = [["SUPPLYDEPOT", "build", 15, 1], ["BARRACKS", "build", 15, 1]]
+    for active in queue:
+        unit_name, action, atSupply, quantity = active
 
-    @staticmethod
-    async def create(bot: BotAI) -> 'BuildManager':
-        instance = BuildManager(bot)
-        await instance.execute_build_queue()
-        return instance
-    
-    async def execute_build_queue(self):
-        queue = [["SUPPLYDEPOT", "build", 15, 1], ["BARRACKS", "build", 15, 1]]
-        for active in queue:
-            unit_name, action, atSupply, quantity = active
-
-            if self.check_prerequisites(unit_name, quantity):
-                await self.build_unit(unit_name)
-            else:
-                continue
-            
-            print(queue[0])
-            queue.remove(active) #not working
-
-    #check prerequisites(minerals/gas, under construction, already existing)
-    def check_prerequisites(self, unit_name, quantity):
-        if self.bot.can_afford(UnitTypeId[unit_name]) and not self.bot.structures(UnitTypeId[unit_name]):
-            return True
+        if self.check_prerequisites(unit_name, quantity):
+            await self.build_unit(unit_name)
         else:
-            return False  
+            continue
         
-    def build_progress(self):
-        structures = self.bot.structures
-        structure_counts = {}
+        print(queue[0])
+        queue.remove(active) #not working
 
-        for unit in structures:
-            if unit.name in structure_counts:
-                structure_counts[unit.name] += 1
-            else:
-                structure_counts[unit.name] = 1
-
-        sorted_structure_counts = sorted(structure_counts.items())
-
-        print(sorted_structure_counts)  #debug
-        #return sorted_structure_counts 
+#check prerequisites(minerals/gas, under construction, already existing)
+def check_prerequisites(self : BotAI, unit_name):
+    if self.can_afford(UnitTypeId[unit_name]):
+        return True
+    else:
+        return False  
     
-    #build request execution
-    async def build_unit(self, unit_name):
-        CCs: Units = self.bot.townhalls(UnitTypeId.COMMANDCENTER)   #remove - debug only / main base logic
-        cc: Unit = CCs.first                                        #remove - debug only / main base logic
-        if self.bot.can_afford(UnitTypeId[unit_name]):
-            await self.bot.build(UnitTypeId[unit_name], near=cc.position.towards(self.bot.game_info.map_center, 8)) #building placement logic missing
+def build_progress(self):
+    structures = self.bot.structures
+    structure_counts = {}
 
-    def getBuildOrder(self):
-        return makeBuildOrder('tools/test.json')
+    for unit in structures:
+        if unit.name in structure_counts:
+            structure_counts[unit.name] += 1
+        else:
+            structure_counts[unit.name] = 1
+
+    sorted_structure_counts = sorted(structure_counts.items())
+
+    print(sorted_structure_counts)  #debug
+    #return sorted_structure_counts 
+
+async def build_next(self : BotAI, buildrequest):
+    #0 unit name, 1 unit id, 2 type of unit, 3 action type, 4 quantity, 5 supply, 6 time, 7 frame
+    buildinfo = str(buildrequest).split(", ")
+    supplyrequired = int(buildinfo[5])
+
+    if self.supply_used < supplyrequired:
+        print("Cannot build, current supply: " + str(self.supply_used))
+        return False
+    
+    unit_name = (str(buildinfo[0]).strip("[")).strip("'").upper
+    #print("checking to see if we can build " + unit_name)
+    if check_prerequisites(self, unit_name) == False:
+        return False
+    else:
+        unit_type = str(buildinfo[2])
+        if unit_type.strip("'") == "structure":
+            build_unit(self, unit_name)
+
+#build request execution
+async def build_unit(self : BotAI, unit_name):
+    CCs: Units = self.townhalls(UnitTypeId.COMMANDCENTER)
+    cc: Unit = CCs.first
+    await self.build(UnitTypeId[unit_name], near=cc.position.towards(self.game_info.map_center, 10)) #building placement logic missing
+    return True
+
+def getBuildOrder(self : BotAI, strategy):
+    build_order = None
+    build_order = makeBuildOrder('tools/' + strategy + '.json')
+    #print("Imported " + len(build_order) + " items into build order")
+    if build_order is not None:
+        return build_order
+    else:
+        return False
--- templateBot.py
+++ templateBot.py
@@ -8,18 +8,79 @@ from sc2.player import Bot, Computer
 from sc2.position import Point2
 from sc2.unit import Unit
 from sc2.units import Units
-from BuildManager import BuildManager
+from BuildManager import getBuildOrder, build_next
 
 #https://burnysc2.github.io/python-sc2/docs/text_files/introduction.html
 
-class CompetitiveBot(BotAI):
+class Jimmy(BotAI):
+    
+    #following code from bot.py from smoothbrain bot as example
+    def __init__(self):
+        self.unit_command_uses_self_do = False
+        self.distance_calculation_method = 2
+        self.game_step: int = 2                      # 2 usually, 6 vs human
+        self.build_starport_techlab_first = True     # always make techlab first on starport, good against dts, skytoss, burrowed roaches, and siege tanks
+        self.worker_rushed = False                   # tells if we are worker rushed, if the enemies were repelled we should close the wall quick before they come back
+        self.out_of_fight_workers = []               # workers with too low hp to defend a worker rush
+        self.scouting_units = []                     # lists units assigned to scout so that we do not cancel their orders
+        self.worker_assigned_to_repair = {}          # lists workers assigned to repair
+        self.worker_assigned_to_follow = {}          # lists workers assigned to follow objects (used to prevent Planetary Fortress rushes)
+        self.worker_assigned_to_defend = {}          # lists workers assigned to defend other workers during construction
+        self.worker_assigned_to_resume_building = {} # lists workers assigned to resume the construction of a building
+        self.worker_assigned_to_expand = {}          # lists workers assigned to expand /!\ not used yet
+        self.townhall_saturations = {}               # lists the mineral saturation of townhalls in queues of 40 frames, we consider the townhall saturated if max_number + 1 >= ideal_number
+        self.refineries_age = {}                     # this is here to tackle an issue with refineries having 0 workers on them when finished, although the building worker is assigned to it
+        self.lifted_cc_pos = {}                      # remember where lifted ccs were
+        self.produce_from_starports = True
+        self.produce_from_factories = True
+        self.produce_from_barracks = True
+        self.scouted_at_time = -1000                 # save moment at which we scouted, so that we don't re-send units every frame
+        self.buildstep = 0
+
+        self.build_order = getBuildOrder(self,'test')    #BuildManager(self)
+
+        super().__init__()
 
     async def on_start(self):
-        print("Game started")
+        #print("Game started")
         # Do things here before the game starts
+        self.CCs: Units = self.townhalls(UnitTypeId.COMMANDCENTER)
+        self.cc: Unit = self.CCs.first
+        #Load build manager once and print once
+        if self.build_order.count != 0:
+            print("successfully loaded build order:")
+            print(self.build_order)
+        else:
+            print("Build order failed to load")
+        
 
-    async def on_step(self, iteration):
-        self.BuildManager = await BuildManager.create(self)
+        # If we don't have a townhall anymore, send all units to attack
+        
+
+    async def on_step(self, iteration: int):
+        #self.BuildManager = await BuildManager.create(self)
+        # if Bob.build_order[buildstep].contains("structure"):
+        #     if self.can_afford(UnitTypeId[Bob.build_order[buildstep]]) and not self.already_pending(UnitTypeId.SUPPLYDEPOT):
+        #         Bob.build_next(Bob.build_order[buildstep])
+
+        if not self.CCs:
+            target: Point2 = self.enemy_structures.random_or(
+                self.enemy_start_locations[0]
+            ).position
+            for unit in self.workers | self.units(UnitTypeId.MARINE):
+                unit.attack(target)
+            return
+        
+        if self.can_afford(UnitTypeId.SCV) and self.supply_workers < 17 and self.cc.is_idle:
+            self.cc.train(UnitTypeId.SCV)
+        
+        #print("Iteration " + str(iteration))
+        if (await build_next(self, str(self.build_order[self.buildstep]).strip("[]"))):
+            self.buildstep = self.buildstep + 1
+        #finally:
+            #print("After doing everything, I'm doing this")
+        #print(Bob.build_order)
+        #Tell Bob to execute build order until we tell him to stop
 
     async def on_end(self):
         print("Game ended.")
@@ -28,7 +89,7 @@ class CompetitiveBot(BotAI):
 def main():
     run_game(
         maps.get("BerlingradAIE"),
-        [Bot(Race.Terran, CompetitiveBot()), Computer(Race.Zerg, Difficulty.Easy)],
+        [Bot(Race.Terran, Jimmy()), Computer(Race.Zerg, Difficulty.Easy)],
         realtime=False,
     )
 
