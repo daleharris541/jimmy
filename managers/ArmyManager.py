f75acadabcf14fb21467dcea95d988862a24469c
Dale Harris <7697179+daleharris541@users.noreply.github.com>
2023-05-09 16:53:15 -0700

    Remove idle check and enabled queue

--- managers/ArmyManager.py
+++ managers/ArmyManager.py
@@ -11,14 +11,17 @@ units_starport = ['VIKING', 'MEDIVAC', 'LIBERATOR', 'RAVEN', 'BANSHEE', 'BATTLEC
 
 async def train_unit(self: BotAI, unit_name):
     if unit_name in units_barracks:
-        for barracks in self.structures(UnitTypeId.BARRACKS).ready.idle:
-            barracks.train(UnitTypeId[unit_name])
+        for barracks in self.structures(UnitTypeId.BARRACKS).ready:
+            if barracks.train(UnitTypeId[unit_name], True):
+                return True
+            else:
+                return False
     elif unit_name in units_factory:
-        for factory in self.structures(UnitTypeId.FACTORY).ready.idle:
-            factory.train(UnitTypeId[unit_name])
+        for factory in self.structures(UnitTypeId.FACTORY).ready:
+            factory.train(UnitTypeId[unit_name], True)
     elif unit_name in units_starport:
-        for starport in self.structures(UnitTypeId.FACTORY).ready.idle:
-            starport.train(UnitTypeId[unit_name])
+        for starport in self.structures(UnitTypeId.FACTORY).ready:
+            starport.train(UnitTypeId[unit_name], True)
     else:
         print(f'ArmyManager: (trainUnit) something went wrong - {unit_name}')
         return False
